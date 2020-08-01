2dbe2d7a887a380d05bf7c9f2dbf830f88d0131b
andreeali1 <toniac91@gmail.com>
2020-08-01 06:28:39 +0000

    cambios

--- hola.py
+++
@@ -1,6 +0,0 @@
-a = int(input("ingresa el valor de a "))
-c = int (input("ingresa el valor de b "))
-if a<c:
-	print("el pirmer valor es mayor")
-else:
-	print ("el segundo valor es mayorque el priemro")
